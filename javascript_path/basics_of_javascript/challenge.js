20e554713475e3fb334af8dbd629ab31e3c1e6a8
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-01 16:50:03 -0600

    Wrote specs for challenge script

---
+++ javascript_path/basics_of_javascript/challenge.js
@@ -0,0 +1,20 @@
+/**
+ * Calculate the total price of a phone purchase.
+ * 
+ * - Keep purchasing phones until you run out of money.
+ * - Buy accessories for each phone as long as the purchase
+ *      amount is below your mental spending threshold.
+ * - After purchase add the tax and pretty print the purchase amount.
+ * - Finally, check the purchase amount against your bank balance
+ *      to see if you can afford it.
+ * - You should have constants for:
+ *  - Tax rate
+ *  - Phone price
+ *  - Spending threshold
+ *  - Accessory price
+ * - You should have a var for bank balance.
+ * - You should have functions for:
+ *  - Tax calculation
+ *  - Printing purchase total
+ * - Ask user to input bank account balance.
+ */
