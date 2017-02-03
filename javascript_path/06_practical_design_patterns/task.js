b560568384db88d6c87ea362efe74be17e759330
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-03 17:04:32 -0600

    Moved save() and complete() to prototype

--- javascript_path/06_practical_design_patterns/task.js
+++ javascript_path/06_practical_design_patterns/task.js
@@ -2,20 +2,23 @@
  * Example of the Constructor pattern
  */
 
+// First we create the constructor function.
 var Task = function (name) {
     this.name = name;
     this.completed = false;
-
-    this.complete = function () {
-        this.completed = true;
-        console.log(`Task completed: ${this.name}`);
-    }    
-
-    this.save = function () {
-        console.log(`Saving task: ${this.name}`);
-    }
 }
 
+// Next we'll add properties to the object prototype
+Task.prototype.complete = function () {
+    this.completed = true;
+    console.log(`Task completed: ${this.name}`);
+}
+
+Task.prototype.save = function () {
+    console.log(`Saving task: ${this.name}`);
+}
+
+// Then we can create copies of the object and us them
 var task1 = new Task("Learn JavaScript patterns");
 var task2 = new Task("Build a demo project");
 var task3 = new Task("Learn React.js");
