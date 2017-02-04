05dea001a9b37086082ae284f6740f50920dd7a7
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-04 12:47:54 -0600

    Added JSDoc to taskRepository.js

--- javascript_path/06_practical_design_patterns/demo/taskRepository.js
+++ javascript_path/06_practical_design_patterns/demo/taskRepository.js
@@ -4,14 +4,30 @@
  * This file is responsible for all of our database calls.
  */
 
+/**
+ * Module to handle DB access.
+ * 
+ * @returns {Object}
+ */
 var repo = function () {
     return {
+        /**
+         * Get a task from the database.
+         * 
+         * @param {number} id
+         * @returns {Object}
+         */
         get: function (id) {
             console.log(`Getting task: ${id}`);
             return {
                 name: "New task from db."
             };
         },
+        /**
+         * Save a task to the database.
+         * 
+         * @param {Object} task
+         */
         save: function (task) {
             console.log(`Saving ${task.name} to the database`);
         }
