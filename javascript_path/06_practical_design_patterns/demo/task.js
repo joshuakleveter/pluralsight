8d08026ff89a2e42811f6ee5a076f7960bd9be86
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-04 12:45:54 -0600

    Added JSDoc to task.js

--- javascript_path/06_practical_design_patterns/demo/task.js
+++ javascript_path/06_practical_design_patterns/demo/task.js
@@ -7,17 +7,29 @@ var Repos = require("./repoFactory");
  */
 
 // First we create the constructor function.
+/**
+ * Constructor for Task objects.
+ * Data parameter takes an object with the name value as a string.
+ * 
+ * @param {Object} data
+ */
 var Task = function (data) {
     this.name = data.name;
     this.completed = false;
 }
 
 // Next we'll add properties to the object prototype
+/**
+ * Call this to complete the task.
+ */
 Task.prototype.complete = function () {
     this.completed = true;
     console.log(`Task completed: ${this.name}`);
 }
 
+/**
+ * Call this to save the task to the database.
+ */
 Task.prototype.save = function () {
     console.log(`Saving task: ${this.name}`);
     Repos.tasks.save(this);
