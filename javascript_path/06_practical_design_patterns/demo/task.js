0bf287e47777c4a891329a444e6ef6062ff10169
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-04 11:26:06 -0600

    Changed to repoFactory API in task.js

--- javascript_path/06_practical_design_patterns/demo/task.js
+++ javascript_path/06_practical_design_patterns/demo/task.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Repo = require("./taskRepository");
+var Repos = require("./repoFactory");
 
 /**
  * Example of the Constructor pattern
@@ -20,7 +20,7 @@ Task.prototype.complete = function () {
 
 Task.prototype.save = function () {
     console.log(`Saving task: ${this.name}`);
-    Repo.save(this);
+    Repos.tasks.save(this);
 }
 
 // Then we can export the object for use in our Node.js app
