764456f43682ecbcb34da48acf1237c8de321ff3
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-04 10:13:01 -0600

    Changed new Task arguments to objects

--- javascript_path/06_practical_design_patterns/demo/main.js
+++ javascript_path/06_practical_design_patterns/demo/main.js
@@ -2,9 +2,9 @@ var Task = require("./task");
 var Repo = require("./taskRepository");
 
 var task1 = new Task(Repo.get(1));
-var task2 = new Task("Build a demo project");
-var task3 = new Task("Learn React.js");
-var task4 = new Task("Learn C#");
+var task2 = new Task({name: 'Build a demo project.'});
+var task3 = new Task({name: 'Learn React.js'});
+var task4 = new Task({name: 'Learn C#'});
 
 task1.complete();
 task2.save();
