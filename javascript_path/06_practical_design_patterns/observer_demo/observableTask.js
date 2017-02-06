42b3f5d578e1e525ad00949f48f4f6f94fda4dc1
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 09:29:31 -0600

    Added .notify() method to ObservableTask.prototype

--- javascript_path/06_practical_design_patterns/observer_demo/observableTask.js
+++ javascript_path/06_practical_design_patterns/observer_demo/observableTask.js
@@ -22,4 +22,17 @@ ObservableTask.prototype.addObserver = function (observer) {
     this.observers.add(observer);
 };
 
+ObservableTask.prototype.notify = function (context) {
+    var observerCount = this.observers.count();
+
+    /**
+     * In real life we would be checking to make sure that our
+     * observers are valid, execuatable functions.
+     */
+
+    for (var i = 0; i < observerCount; i++) {
+        this.observers.get(i)(context);
+    }
+};
+
 module.exports = ObservableTask;
