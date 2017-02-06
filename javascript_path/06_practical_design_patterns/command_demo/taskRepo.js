7c87d1cda5d0058f5890fc5a497769e089e1999c
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 15:20:00 -0600

    Added .execute() method

--- javascript_path/06_practical_design_patterns/command_demo/taskRepo.js
+++ javascript_path/06_practical_design_patterns/command_demo/taskRepo.js
@@ -9,3 +9,13 @@ var repo = {
         console.log(`Saving ${task.name} to the database`);
     }
 };
+
+repo.execute = function (name) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    if (repo[name]) {
+        return repo[name].apply(repo, args);
+    }
+
+    return false;
+};
