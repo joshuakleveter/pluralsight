60c65d9c98263b0a01f56560a94239bf3ec458de
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 11:47:36 -0600

    Changed prototype syntax to object calls.

--- javascript_path/06_practical_design_patterns/observer_demo/main.js
+++ javascript_path/06_practical_design_patterns/observer_demo/main.js
@@ -13,12 +13,12 @@ var notifier = new NotificationService();
 var logger = new LoggingService();
 var auditor = new AuditingService();
 
-task1.addObserver(NotificationService.prototype.update.bind(notifier));
-task1.addObserver(LoggingService.prototype.update.bind(logger));
-task1.addObserver(AuditingService.prototype.update.bind(auditor));
+task1.addObserver(notifier.update.bind(notifier));
+task1.addObserver(logger.update.bind(logger));
+task1.addObserver(auditor.update.bind(auditor));
 
 task1.save();
 
-task1.removeObserver(AuditingService.prototype.update.bind(auditor));
+task1.removeObserver(auditor.update.bind(auditor));
 
 task1.save();
