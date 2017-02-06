96019e3682d7ee9877dc86218b1bd1eaa72c57d3
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 08:44:02 -0600

    Added auditingService

--- javascript_path/06_practical_design_patterns/observer_demo/main.js
+++ javascript_path/06_practical_design_patterns/observer_demo/main.js
@@ -20,6 +20,16 @@ var loggingService = function () {
     };
 };
 
+/**
+ * Audit a task's status.
+ */
+var auditingService = function () {
+    var message = "Auditing: ";
+    this.update = function (task) {
+        console.log(`${message} ${task.user} for task ${task.name}`);
+    };
+};
+
 var task1 = new Task({
     name: "Demo Task",
     user: "Josh"
