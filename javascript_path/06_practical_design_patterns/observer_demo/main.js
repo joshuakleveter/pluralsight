57547bfa5f4706366dca87449edd7abdc34cfc14
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 08:42:06 -0600

    Added notificationService

--- javascript_path/06_practical_design_patterns/observer_demo/main.js
+++ javascript_path/06_practical_design_patterns/observer_demo/main.js
@@ -1,5 +1,15 @@
 var Task = require("./task");
 
+/**
+ * Notify a user of task chagnes.
+ */
+var notificationService = function () {
+    var message = "Notifying ";
+    this.update = function (task) {
+        console.log(`${message} ${task.user} for task ${task.name}`);
+    };
+};
+
 var task1 = new Task({
     name: "Demo Task",
     user: "Josh"
