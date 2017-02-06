e126b7c8ec153ecfb915bf2f36aab9bc6915b190
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 09:11:26 -0600

    Moved ObserverList to a module.

--- javascript_path/06_practical_design_patterns/observer_demo/main.js
+++ javascript_path/06_practical_design_patterns/observer_demo/main.js
@@ -30,39 +30,6 @@ var auditingService = function () {
     };
 };
 
-/**
- * List of obervers for Task objects.
- */
-var ObserverList = function () {
-    this.observerList = [];
-};
-
-/**
- * Add a new observer to the ObserverList
- * 
- * @param   {Object} observer Object to add as an observer.
- * @returns {number}        Length of the ObserverList.
- */
-ObserverList.prototype.add = function (observer) {
-    return this.observerList.push(observer);
-};
-
-/**
- * Get an observer from ObserverList.
- * 
- * @param   {number} index index of the observer to get.
- * @returns {Object}     The requested observer object.
- *                       Returns -1 if the requested index does not exist.
- */
-ObserverList.prototype.get = function (index) {
-    if (index >= 0 && index < this.observerList.length) {
-        return this.observerList[index];
-    }
-    else {
-        return -1;
-    }
-};
-
 var task1 = new Task({
     name: "Demo Task",
     user: "Josh"
