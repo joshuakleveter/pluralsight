9eb6033311c463bdb45ade3637e2f9c92c0d51f2
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-06 09:03:58 -0600

    Added .get() method to ObserverList.prototype

--- javascript_path/06_practical_design_patterns/observer_demo/main.js
+++ javascript_path/06_practical_design_patterns/observer_demo/main.js
@@ -40,11 +40,27 @@ var ObserverList = function () {
 /**
  * Add a new observer to the ObserverList
  * 
- * @param {Object} observer Object to add as an observer.
+ * @param   {Object} observer Object to add as an observer.
  * @returns {number}        Length of the ObserverList.
  */
 ObserverList.prototype.add = function (observer) {
     return this.observerList.push(observer);
+};
+
+/**
+ * Get an observer from ObserverList.
+ * 
+ * @param   {number} index index of the observer to get.
+ * @returns {Object}     The requested observer object.
+ *                       Returns -1 if the requested index does not exist.
+ */
+ObserverList.prototype.get = function (index) {
+    if (index >= 0 && index < this.observerList.length) {
+        return this.observerList[index];
+    }
+    else {
+        return -1;
+    }
 };
 
 var task1 = new Task({
