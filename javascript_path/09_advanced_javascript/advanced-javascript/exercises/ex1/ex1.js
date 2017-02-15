aec3263ccec010e1e092e45af527af447b963b62
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-15 16:20:42 -0600

    Finished exercise 1

--- javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex1/ex1.js
+++ javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex1/ex1.js
@@ -1,77 +1,74 @@
-A();
-
-function C() {
-	console.log("OOPS!");
-}
-
-function E(f) {
-	console.log("E");
-	f();
-	var f = F;
-}
-
-var A = function() {
-	console.log("A");
-	B();
-};
-
-var C;
-
-function G() {
-	console.log("G");
-	H();
-
-	var H = function() {
-		console.log("H");
-		I();
-	};
-}
-
-var D = d;
-
-function d() {
-	console.log("D");
-	E();
-}
-
-function I() {
-	console.log("I");
-	J();
-	J();
-}
-
-B = function() {
-	console.log("B");
-	C();
-};
-
-var F = function() {
-	console.log("F");
-	G();
-};
-
-var rest = "KLMNOPQRSTUVWXYZ".split("");
-for (var i=0; i<rest.length; i++) {
-	(function(i){
-		// define the current function
-		window[rest[i]] = function() {
-			console.log(rest[i]);
-			if (i < (rest.length-1)) {
-				// TODO: call the next function
-			}
-		};
-	})(i);
-}
-
-var J = function() {
-	J = function() {
-		console.log("J");
-		K();
-	};
-};
-
-C = function() {
-	console.log("C");
-	D();
-};
-
+(function IIFE() {
+	A();
+	
+	function C() {
+		console.log("OOPS!");
+	}
+	
+	function E(f) {
+		console.log("E");
+		F();
+		var f = F;
+	}
+	
+	function A() {
+		console.log("A");
+		B();
+	}
+	
+	function G() {
+		console.log("G");
+		H();
+		
+		function H() {
+			console.log("H");
+			I();
+		}
+	}
+	
+	function D() {
+		console.log("D");
+		E();
+	}
+	
+	function I() {
+		console.log("I");
+		J();
+	}
+	
+	function B() {
+		console.log("B");
+		C();
+	}
+	
+	function F() {
+		console.log("F");
+		G();
+	}
+	
+	var rest = "KLMNOPQRSTUVWXYZ".split("");
+	for (var i=0; i<rest.length; i++) {
+		(function IIFE(i){
+			// define the current function
+			window[rest[i]] = function() {
+				console.log(rest[i]);
+				if (i < rest.length-1) {
+					// TODO: call the next function
+				}
+			};
+			window[rest[i]]();
+		})(i);
+	}
+	
+	function J() {
+		(function() {
+			console.log("J");
+			// K();
+		})();
+	}
+	
+	function C() {
+		console.log("C");
+		D();
+	}
+})();
