3f4e4523ab66c304a9f435d1c6f938e5628476e2
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-20 11:18:41 -0600

    Wrote solution to ex4 - part 1

--- javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex4/ex4.js
+++ javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex4/ex4.js
@@ -1,41 +1,54 @@
 function Widget(width,height) {
-	this.width = width || 50;
-	this.height = height || 50;
-	this.$elem = null;
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
 }
 
 Widget.prototype.render = function($where){
-	if (this.$elem) {
-		this.$elem.css({
-			width: this.width + "px",
-			height: this.height + "px"
-		}).appendTo($where);
-	}
+    if (this.$elem) {
+        this.$elem.css({
+            width: this.width + "px",
+            height: this.height + "px"
+        }).appendTo($where);
+    }
 };
 
-function Button(/* ... */) {
-/*
-	...
-	this.$elem = $("<button>").text(this.label);
-*/
+function Button(opts) {
+    this.height = opts.height;
+    this.label = opts.label;
+    this.width = opts.width;
+    this.$elem = $("<button>").text(this.label);
 }
+Button.prototype = Object.create(Widget.prototype);
+Button.prototype.constructor = Button.prototype;
 
-/*
-Button -> render = function($where) {
-	// call the parent render()
-	// add a click handler -> onClick
-}
+Button.prototype.render = function($where) {
+    // call the parent render()
+    Widget.prototype.render.call(this, $where);
 
-Button -> onClick = function(evt) {
-	console.log("...");
-}
+    // add a click handler -> onClick
+    this.$elem.on('click', event => {
+        this.onClick(event);
+    });
+};
+
+Button.prototype.onClick = function(evt) {
+    console.log(`Button ${this.label} clicked!`);
+};
 
 $(document).ready(function(){
-	var $body = $(document.body);
-	var btn1 = ...;
-	var btn2 = ...;
+    var $body = $(document.body);
+    var btn1 = new Button({
+        label: "Button 1",
+        height: 100,
+        width: 200
+    });
+    var btn2 = new Button({
+        label: "Button 2",
+        height: 50,
+        width: 150
+    });
 
-	btn1.render($body);
-	btn2.render($body);
+    btn1.render($body);
+    btn2.render($body);
 });
-*/
