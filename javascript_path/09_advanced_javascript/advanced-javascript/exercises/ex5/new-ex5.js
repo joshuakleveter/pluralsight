386ced8810e2479834ac8eae8de715a61e48d68e
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-21 07:49:12 -0600

    Added overview

---
+++ javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex5/new-ex5.js
@@ -0,0 +1,13 @@
+/**
+ * Structure:
+ * 
+ * - Ajax module
+ * 	- Handle AJAX calls
+ * 
+ * - FileLoader module
+ * 	- getFiles() generator
+ * 		- Take an Array of filenames
+ * 		- Load cocurrently
+ * 		- Render in same order as array
+ * 	- renderFile()
+ */
