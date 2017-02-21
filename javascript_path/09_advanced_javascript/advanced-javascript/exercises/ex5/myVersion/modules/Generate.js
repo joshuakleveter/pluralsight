8b94422e3ecb90b0254c5d097af8d6dca2a403de
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-21 12:04:39 -0600

    Added code for Generate module

--- javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex5/myVersion/modules/Generate.js
+++ javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex5/myVersion/modules/Generate.js
@@ -1,5 +1,20 @@
-// Imports ---------------------------------------------------------------------
-
 // Public API ------------------------------------------------------------------
 
+function run(generator, args) {
+    var response = generator.next(args);
+    
+    while (false == response.done) {
+        response.value.then(
+            function resolve(value) {
+                generator.next(value);
+            },
+            function reject(reason) {
+                generator.next(reason);
+            }
+        );
+    }
+}
+
 // Exports ---------------------------------------------------------------------
+
+module.exports = run;
