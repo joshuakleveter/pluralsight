3fc19193eeef284e6a1ecce164ba9042618adc90
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-02-21 11:45:50 -0600

    Added Logger module

---
+++ javascript_path/09_advanced_javascript/advanced-javascript/exercises/ex5/myVersion/modules/Logger.js
@@ -0,0 +1,14 @@
+// Public API ------------------------------------------------------------------
+
+/**
+ * Log text to the console.
+ * 
+ * @param {string} text What we should log to the console.
+ */
+function logText(text) {
+    console.log(text);
+}
+
+// Exports ---------------------------------------------------------------------
+
+module.exports = logText;
