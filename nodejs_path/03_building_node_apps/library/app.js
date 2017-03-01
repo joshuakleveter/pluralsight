d253b7b69cc425320a87b2e52cccf60910e4646c
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-03-01 15:20:48 -0600

    Added code and demos from module 2

--- nodejs_path/03_building_node_apps/library/app.js
+++ nodejs_path/03_building_node_apps/library/app.js
@@ -1,9 +1,20 @@
 var express = require('express');
 
-var hi = 'Hello!';
+var app = express();
 
-if (true) {
-    console.log('hi');
-}
+var port = 5000;
 
-console.log(hi);
+app.listen(port, function(err) {
+    console.log(`Running server on port ${port}`);
+});
+
+// .use sets up middleware
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
+app.get('/books', function(request, response) {
+    response.send('Hello Books.');
+});
